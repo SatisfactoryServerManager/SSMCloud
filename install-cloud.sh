b123d6e4b9a6f69271bb095d18662d4975a668f9
mrhid6 <dominic.richardson1@hotmail.co.uk>
2023-02-14 22:04:46 +0000

    chore(CI): UpdateScript

--- install-cloud.sh
+++ install-cloud.sh
@@ -31,6 +31,29 @@ INSTALL_DIR="/opt/SSM"
 SSM_SERVICENAME="SSM.service"
 SSM_SERVICEFILE="/etc/systemd/system/SSM.service"
 
+QUIET=0;
+SKIPMONGO=0;
+UPDATE=0;
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+    -q)
+        QUIET=1
+        shift # past value
+        ;;
+    --skipmongo)
+        SKIPMONGO=1
+        shift # past value
+        ;;
+    -u)
+        UPDATE=1
+        shift # past value
+        ;;
+    esac
+done
+
 
 if [ ! "${PLATFORM}" == "Linux" ]; then
     echo -e "${RED}Error: Install Script Only Works On Linux Platforms!${NC}"
@@ -54,22 +77,25 @@ function _spinner() {
 
     case $1 in
         start)
-            # calculate the column where spinner and status msg will be displayed
-            let column=$(tput cols)-${#2}-8
-            # display message and position the cursor in $column column
-            echo -ne ${2}
-            printf "%${column}s"
 
-            # start spinner
-            i=1
-            sp='\|/-'
-            delay=${SPINNER_DELAY:-0.15}
+            if [ $QUIET -eq 0 ]; then
+                # calculate the column where spinner and status msg will be displayed
+                let column=$(tput cols)-${#2}-8
+                # display message and position the cursor in $column column
+                echo -ne ${2}
+                printf "%${column}s"
 
-            while :
-            do
-                printf "\b${sp:i++%${#sp}:1}"
-                sleep $delay
-            done
+                # start spinner
+                i=1
+                sp='\|/-'
+                delay=${SPINNER_DELAY:-0.15}
+
+                while :
+                do
+                    printf "\b${sp:i++%${#sp}:1}"
+                    sleep $delay
+                done
+            fi
             ;;
         stop)
             if [[ -z ${3} ]]; then
@@ -79,14 +105,16 @@ function _spinner() {
 
             kill $3 > /dev/null 2>&1
 
-            # inform the user uppon success or failure
-            echo -en "\b["
-            if [[ $2 -eq 0 ]]; then
-                echo -en "${green}${on_success}${nc}"
-            else
-                echo -en "${red}${on_fail}${nc}"
+            if [ $QUIET -eq 0 ]; then
+                # inform the user uppon success or failure
+                echo -en "\b["
+                if [[ $2 -eq 0 ]]; then
+                    echo -en "${green}${on_success}${nc}"
+                else
+                    echo -en "${red}${on_fail}${nc}"
+                fi
+                echo -e "]"
             fi
-            echo -e "]"
             ;;
         *)
             echo "invalid argument, try {start/stop}"
@@ -126,21 +154,35 @@ apt-get -qq install apt-utils curl wget jq binutils software-properties-common l
 apt-get -qq update -y >/dev/null 2>&1
 stop_spinner $?
 
-
-read -r -p "Do you want to install MongoDB? [y/N]: " response </dev/tty
-case $response in
-[yY]*)
-  start_spinner "${YELLOW}Installing MongoDB 5.0${NC}"
-  apt-get install gnupg > /dev/null
-  wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add - > /dev/null 2>&1
-  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list > /dev/null 2>&1
-  apt-get update > /dev/null;
-  apt-get install -y mongodb-org mongodb-org-shell > /dev/null;
-  stop_spinner $?
-  systemctl daemon-reload > /dev/null 2>&1
-  systemctl enable mongod > /dev/null 2>&1
+INSTALLMONGO=0;
 
 
+if [ $QUIET -eq 0 ]; then
+    read -r -p "Do you want to install MongoDB? [y/N]: " response </dev/tty
+    case $response in
+    [yY]*)
+        INSTALLMONGO=1
+    ;;
+    *)
+        INSTALLMONGO=0;
+    ;;
+    esac
+fi
+
+if [ $SKIPMONGO -eq 1 ]; then
+    INSTALLMONGO=0;
+fi
+
+if [ $INSTALLMONGO -eq 1 ]; then
+    start_spinner "${YELLOW}Installing MongoDB 5.0${NC}"
+    apt-get install gnupg > /dev/null
+    wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add - > /dev/null 2>&1
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list > /dev/null 2>&1
+    apt-get update > /dev/null;
+    apt-get install -y mongodb-org mongodb-org-shell > /dev/null;
+    stop_spinner $?
+    systemctl daemon-reload > /dev/null 2>&1
+    systemctl enable mongod > /dev/null 2>&1
 
     start_spinner "${YELLOW}Waiting for MongoDB to start...${NC}"
     service mongod start > /dev/null 2>&1
@@ -166,29 +208,34 @@ db.dropAllUsers();
 db.createUser({"user": "ssm", "pwd": "#SSMPa$£", "roles": ["readWrite", "dbAdmin"]});
 EOL
   mongo /etc/mongosetup_ssm.js > /dev/null 2>&1
-  ;;
-*)
-  echo -e "${RED}MongoDB install skipped...${NC}"
-  start_spinner "${YELLOW}Installing MongoDB Tools...${NC}"
-  wget https://repo.mongodb.org/apt/ubuntu/dists/focal/mongodb-org/5.0/multiverse/binary-amd64/mongodb-org-database-tools-extra_5.0.6_amd64.deb > /dev/null 2>&1;
-  dpkg -i mongodb-org-database-tools-extra_5.0.6_amd64.deb > /dev/null 2>&1;
-  rm -rf mongodb-org-database-tools-extra_5.0.6_amd64.deb > /dev/null 2>&1
-  stop_spinner $?
-  ;;
-esac
+else
+    if [ $QUIET -eq 0 ]; then
+        echo -e "${RED}MongoDB install skipped...${NC}"
+    fi
+    start_spinner "${YELLOW}Installing MongoDB Tools...${NC}"
+    wget https://repo.mongodb.org/apt/ubuntu/dists/focal/mongodb-org/5.0/multiverse/binary-amd64/mongodb-org-database-tools-extra_5.0.6_amd64.deb > /dev/null 2>&1;
+    dpkg -i mongodb-org-database-tools-extra_5.0.6_amd64.deb > /dev/null 2>&1;
+    rm -rf mongodb-org-database-tools-extra_5.0.6_amd64.deb > /dev/null 2>&1
+    stop_spinner $?
+  
+fi
 
 
 if [ -d "${INSTALL_DIR}" ]; then
 
-    read -r -p "Do you want to update SSM? [y/N]: " response </dev/tty
-    case $response in
-    [yY]*)
-    ;;
-    *)
-        echo -e "${RED}Update Skipped.. Exiting ${NC}"
-        exit 1
-    ;;
-    esac
+    if [ $QUIET -eq 0 ]; then
+        read -r -p "Do you want to update SSM? [y/N]: " response </dev/tty
+        case $response in
+        [yY]*)
+        ;;
+        *)
+            echo -e "${RED}Update Skipped.. Exiting ${NC}"
+            exit 1
+        ;;
+        esac
+    elif [ $UPDATE -eq 0 ]; then
+        exit 1;
+    fi
 else
     mkdir -p ${INSTALL_DIR} >/dev/null 2>&1
 fi
@@ -289,3 +336,5 @@ systemctl daemon-reload >/dev/null 2>&1
 systemctl enable ${SSM_SERVICENAME} >/dev/null 2>&1
 systemctl start ${SSM_SERVICENAME} >/dev/null 2>&1
 stop_spinner $?
+
+exit 0
