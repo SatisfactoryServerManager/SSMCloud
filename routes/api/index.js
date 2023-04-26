3eaffe897238db9eaee577c441a4cbf3b52106e0
mrhid6 <dominic.richardson1@hotmail.co.uk>
2023-04-26 16:28:29 +0100

    feat: ping api endpoint

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/ping", (req, res) => {
+    res.status(200).json({
+        success: true,
+    });
+});
+
 router.use("/agent", require("./agent"));
 router.use("/v1", require("./v1"));
 
